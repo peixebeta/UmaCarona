3054f19ecb73fb43ddd1fdfbcce98b97acf36113
Tousten <54995011+Tousten@users.noreply.github.com>
2019-09-13 20:24:02 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<meta charset="utf-8">
+<link rel="stylesheet" href="bootstrap431/css/bootstrap.min.css">
+<html>
+<head>
+	<title>Totalmente Seguro e Funcional - Site de Caronas</title>
+</head>
+<body>
+  <center>
+	<div class=jumbotron text-center";>
+<form >
+  <div class="form-row" >
+    <div class="col-4">
+      <input type="text" class="form-control" placeholder="Origem">
+    </div>
+    <div class="col-4">
+      <input type="text" class="form-control" placeholder="Destino">
+    </div>
+    <div class="col-2">
+      <input type="text" class="form-control"   placeholder="Saida (opcional)" >
+                
+            </div>
+  </div>
+</form>
+</div>	
+	</div>
+  </center>
+</body>
+</html>
